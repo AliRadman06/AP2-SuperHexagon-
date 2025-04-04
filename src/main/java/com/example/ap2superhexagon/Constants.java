83ee826b2f4e0d2a4167d2925494510888631bcf
Ali Radman <radmanali8490@gmail.com>
2025-04-04 17:27:16 +0430

    Create initail game class structure an FXML files

---
+++ src/main/java/com/example/ap2superhexagon/Constants.java
@@ -0,0 +1,4 @@
+package com.example.ap2superhexagon;
+
+public class Constants {
+}
---
+++ src/main/java/com/example/ap2superhexagon/Game.java
@@ -0,0 +1,7 @@
+package com.example.ap2superhexagon;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Game {
+}
