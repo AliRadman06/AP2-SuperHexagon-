3a264243485136e7adb91df99783578c842bc4d2
Ali Radman <radmanali8490@gmail.com>
2025-04-04 16:22:32 +0430

    Initial project structure setup (Maven JavaFX)

---
+++ src/main/java/com/example/ap2superhexagon/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.ap2superhexagon;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
