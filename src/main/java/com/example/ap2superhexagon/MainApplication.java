0683543f9aacd79685248b4cd2e915be4286957a
Ali Radman <radmanali8490@gmail.com>
2025-04-05 05:02:35 +0430

    MainApplication edited

--- src/main/java/com/example/ap2superhexagon/MainApplication.java
+++ src/main/java/com/example/ap2superhexagon/MainApplication.java
@@ -2,14 +2,35 @@ package com.example.ap2superhexagon;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class MainApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
+        try {
+            URL fxmlLocation = getClass().getResource("/com/example/ap2superhexagon/main-menu-view.fxml");
+            if (fxmlLocation == null) {
+                System.err.println("Could not find main-menu-view.fxml");
+                return;
+            }
+
+            FXMLLoader fxmlLoader = new FXMLLoader(fxmlLocation);
+            Parent root = fxmlLoader.load();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("SuperHexagon-");
+            stage.show();
+        }
+
+        catch (IOException e) {
+            System.err.println("Could not load main-menu-view.fxml");
+        }
     }
 
     public static void main(String[] args) {
