4ba8d9f5fcead2c0b20a9f7a8b5b4f29ef797435
Ali Radman <radmanali8490@gmail.com>
2025-04-06 04:50:39 +0330

    handleStartButton edited

--- src/main/java/com/example/ap2superhexagon/MainMenuController.java
+++ src/main/java/com/example/ap2superhexagon/MainMenuController.java
@@ -2,8 +2,15 @@ package com.example.ap2superhexagon;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainMenuController {
     @FXML
@@ -24,6 +31,18 @@ public class MainMenuController {
     @FXML
     void handleStartButton(ActionEvent event) {
         System.out.println("Start button clicked!");
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/ap2superhexagon/PreGameSetup.fxml"));
+            Parent perGameRoot = fxmlLoader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(perGameRoot);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (IOException e) {
+            System.err.println("Failed to load PreGameSetup.fxml");
+            e.printStackTrace();
+        }
         // کد رفتن به صفحه بازی
     }
 
